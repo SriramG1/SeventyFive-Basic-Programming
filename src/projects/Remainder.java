05604255210d3bf159e38c71edf141cade99e82e
SriramG1 <sriramhht@gmail.com>
2022-02-04 23:29:12 +0530

    'project' 'project'

---
+++ src/projects/Remainder.java
@@ -0,0 +1,18 @@
+package projects;
+
+import java.util.Scanner;
+
+public class Remainder {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter Your Dividend :");
+        int Dividend=in.nextInt();
+        System.out.println("Enter the divisor: ");
+        int divisor=in.nextInt();
+
+        int quotient=Dividend/divisor;
+        int remainder= (Dividend%divisor);
+        System.out.println("Your Remainder value is : "+remainder);
+        System.out.println("Your Quotient value is : "+quotient);
+    }
+}
