9e04634093c37ce9582a53f30022cd562227e689
SriramG1 <sriramhht@gmail.com>
2022-02-04 23:05:58 +0530

    'project' 'project'

---
+++ src/projects/Fifteen.java
@@ -0,0 +1,26 @@
+package projects;
+
+import java.util.Scanner;
+
+public class Fifteen {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the value of a , b and c : ");
+        double a=in.nextFloat();
+        double b=in.nextFloat();
+        double c=in.nextFloat();
+        double x=b*b-4.0*a*c;
+        if (x>0.0) {
+            double root1 =  (-b + Math.pow(x,0.5)) / (2 * a);
+            double root2 =  (-b - Math.pow(x,0.5)) / (2 * a);
+            System.out.println("Roots are " + root1 + " and " + root2);
+        }
+        else if(x==0){
+            double root =-b/(2.0*a);
+            System.out.println("Root is " + root);
+        }
+        else{
+            System.out.println("Roots are not Real");
+        }
+    }
+}
