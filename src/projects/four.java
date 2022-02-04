8f4f596d5d4ac01fc23c393defeeef5a355685f3
SriramG1 <sriramhht@gmail.com>
2022-02-04 21:40:52 +0530

    'project' 'project'

--- src/projects/four.java
+++ src/projects/four.java
@@ -1,6 +1,6 @@
 package projects;
 
-public class program_4 {
+public class four {
     public static void main(String[] args) {
         //Write a program to check if the following numbers are same or different ? input: 6.02f,6.02d
         float float_value = 6.02f;
