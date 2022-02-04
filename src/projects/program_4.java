32e714269cc76890efd440df243d1227183f9139
SriramG1 <sriramhht@gmail.com>
2022-02-04 21:09:54 +0530

    'project' 'project'

--- src/projects/program_4.java
+++ src/projects/program_4.java
@@ -5,6 +5,8 @@ public class program_4 {
         //Write a program to check if the following numbers are same or different ? input: 6.02f,6.02d
         float float_value = 6.02f;
         double double_value = 6.02d;
+        System.out.println(" ");
         System.out.println(float_value == double_value);
+
     }
 }
