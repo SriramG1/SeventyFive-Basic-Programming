66295160297c8ac137d1e2f3aeb08d0d6884f424
SriramG1 <sriramhht@gmail.com>
2022-02-05 17:50:25 +0530

    'project' 'project'

---
+++ src/projects/FibonacciSequence.java
@@ -0,0 +1,19 @@
+package projects;
+
+import java.util.Scanner;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        System.out.println("Enter the Limit :");
+        int n=in.nextInt();
+        int a=-1,b=1,c;
+        for(int i=1;i<=n;i++)
+        {
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }
+    }
+}
