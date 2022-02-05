2916a445a0cab6983a1fe793eefe60b309953b86
SriramG1 <sriramhht@gmail.com>
2022-02-05 11:29:34 +0530

    'project' 'project'

---
+++ src/projects/Calculator.java
@@ -0,0 +1,45 @@
+package projects;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Select any operator : ");
+        Scanner in = new Scanner(System.in);
+        System.out.println("1.Addition");
+        System.out.println("2.Subtraction");
+        System.out.println("3.Multiplication");
+        System.out.println("4.Division");
+        System.out.println("5.Remainder");
+        System.out.println("Enter your choice :");
+        int choice= in.nextInt();
+        System.out.println("Enter two values :");
+        int num1= in.nextInt();
+        int num2= in.nextInt();
+        switch (choice)
+        {
+            case 1:
+                int result=num1+num2;
+                System.out.println("Your addition value is : "+result);
+                break;
+            case 2:
+                 result=num1-num2;
+                System.out.println("Your subtraction value is "+result);
+                break;
+            case 3:
+                 result=num1*num2;
+                System.out.println("Your multiplication value is "+result);
+                break;
+            case 4:
+                result=num1/num2;
+                System.out.println("Your division value is "+result);
+                break;
+            case 5:
+                result=num1%num2;
+                System.out.println("Your remainder value is "+result);
+                break;
+            default:
+                System.out.println("Enter valid option !");
+        }
+    }
+}
