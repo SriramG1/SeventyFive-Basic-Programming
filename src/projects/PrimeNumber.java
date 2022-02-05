2878ef92f205393b77f1aba898103790a870fe2b
SriramG1 <sriramhht@gmail.com>
2022-02-05 11:11:51 +0530

    'project' 'project'

---
+++ src/projects/PrimeNumber.java
@@ -0,0 +1,24 @@
+package projects;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter Your number : ");
+        int number= in.nextInt();
+        int count=0;
+        for(int i=1;i<=number;i++)
+        {
+            if(number%i==0) {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            System.out.println(number+" is prime number");
+        }
+        else
+            System.out.println(number+" is not a prime number");
+    }
+}
