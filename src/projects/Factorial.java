3b49e64c4fa426dc6fbb53bf4ed71e65d8fbec4b
SriramG1 <sriramhht@gmail.com>
2022-02-05 08:07:52 +0530

    'project' 'project'

---
+++ src/projects/Factorial.java
@@ -0,0 +1,17 @@
+package projects;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter your number :");
+        int number=in.nextInt();
+        int fact=1;
+        for(int i=1;i<=number;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+ " is "+ fact);
+    }
+}
