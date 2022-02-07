12383082047b85a32a176a3dca9688a0252ec6b2
SriramG1 <sriramhht@gmail.com>
2022-02-07 15:21:56 +0530

    'project' 'project'

--- src/projects/RecursionPower.java
+++ src/projects/RecursionPower.java
@@ -3,12 +3,22 @@ package projects;
 import java.util.Scanner;
 
 public class RecursionPower {
-    public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        System.out.println("Enter Power value :");
-        int number=in.nextInt();
-        System.out.println("Enter Base value :");
-        int number1=in.nextInt();
+    public static int rec(int base, int power){
+        if(power==0) {
+            return 1;
+        }
+        else {
+            return base * rec(base, power - 1);
+        }
+    }
 
+    public static void main(String[] args) {
+     Scanner in =new Scanner(System.in);
+        System.out.println("Enter Your Base value : ");
+        int base=in.nextInt();
+        System.out.println("Enter Your Power value : ");
+        int power=in.nextInt();
+        int result = rec(base,power);
+        System.out.println("value of "+base+ " ^ "+power+" : "+result);
     }
 }
