59c7f65d88e847ce4cd361286446caab7be2284a
SriramG1 <sriramhht@gmail.com>
2022-02-07 19:58:34 +0530

    'project' 'project'

---
+++ src/projects/DemonstrateLong.java
@@ -0,0 +1,15 @@
+package projects;
+
+public class DemonstrateLong {
+    public static void main(String[] args) {
+        //Program : 30
+        //Program to Demonstrate the working of keyword long
+
+        System.out.println("Long maximum and minimum values :");
+        long max= 9223372036854775807L;
+        long min= -9223372036854775808L;
+        System.out.println("Maximum :  "+max);
+        System.out.println("Maximum : "+min);
+        System.out.println();
+    }
+}
