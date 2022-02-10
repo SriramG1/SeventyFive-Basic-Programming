229d8e473b62c9e0691eb7be38411ce84083558c
SriramG1 <sriramhht@gmail.com>
2022-02-10 11:43:48 +0530

    'Changing format type for output'

--- src/projects/Eleven.java
+++ src/projects/Eleven.java
@@ -4,6 +4,7 @@ public class Eleven {
     public static void main(String[] args) {
         //Write a program to print the number 7.50 without using String.
         float myFloat =7.50f;
-        System.out.printf("%0.2f",myFloat);
+        System.out.printf("%1.2f",myFloat);
+        System.out.println();
     }
 }
