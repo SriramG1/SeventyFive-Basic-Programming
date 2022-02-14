345d75cf00ee05af4434d8ccce24b65fa4c8b9bf
SriramG1 <sriramhht@gmail.com>
2022-02-14 14:04:18 +0530

    'find ASCII value for your character'

--- src/projects/AsciiValue.java
+++ src/projects/AsciiValue.java
@@ -8,10 +8,8 @@ public class AsciiValue {
         //Write a program to Find a ASCII value of a Character
 
         Scanner in=new Scanner(System.in);
-        System.out.println("Enter your Number : ");
-        int c= in.nextInt();
-        System.out.println("ASCII value for " + c + " is " + (char)c);
-        System.out.println("Enter your Character ");
-
+        System.out.println("Enter your Character : ");
+        char character= in.next().charAt(0);
+        System.out.println("ASCII value of " + character + " is --> " + (int)character);
     }
 }
