c53e89caa43e8e392ab1b257ceaaa2c3f92d9163
SriramG1 <sriramhht@gmail.com>
2022-02-15 17:13:05 +0530

    'Find given number is prime or not'

--- src/projects/PrimeNumber.java
+++ src/projects/PrimeNumber.java
@@ -7,22 +7,24 @@ public class PrimeNumber {
         //program : 49
         //program to check whether a number is prime or not:
 
-        Scanner in=new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.println("Enter Your number : ");
-        int number= in.nextInt();
-        int count=0;
-        for(int i=2;i<number;i++)
-        {
-            if(number%i==0) {
-                count++;
-                break;
+        int number = in.nextInt();
+        int count = 0;
+        if (number == 0 || number == 1) {
+            System.out.println(number + " is not a prime number");
+        }
+        else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    count++;
+                    break;
+                }
             }
+            if (count == 0) {
+                System.out.println(number + " is prime number");
+            } else
+                System.out.println(number + " is not a prime number");
         }
-        if(count==0)
-        {
-            System.out.println(number+" is prime number");
-        }
-        else
-            System.out.println(number+" is not a prime number");
     }
 }
